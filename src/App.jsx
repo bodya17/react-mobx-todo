6b9596d7bd7448aab764319994b29106052ec1b0
Volodymyr Shevchuk <vshevchuk@eagleinvsys.com>
2017-12-01 18:05:12 +0200

    'first'

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Header from './components/Header';
+// import FilterContainer from './components/FilterContainer';
+// import ListContainer from './components/ListContainer';
+// import FormContainer from './components/FormContainer';
+
+function App() {
+    return (
+        <main>
+            todolist
+            <Header />
+            {/* <HeaderContainer />
+            <FilterContainer />
+            <ListContainer />
+            <FormContainer /> */}
+        </main>
+    );
+}
+
+export default App;
