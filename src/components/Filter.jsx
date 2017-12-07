8276bd794b0d9d1add1d1c2dc45e3ecc96221573
bodya17 <vshevchuk@eagleinvsys.com>
2017-12-07 16:06:56 +0200

    move constants to variables

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
+import { ALL, COMPLETED, UNCOMPLETED } from '../constants/filters';
 
 import FilterLink from './FilterLink';
 
@@ -7,20 +8,20 @@ const Filter = props => (
   <div className="todo-filter">
     <FilterLink
       icon="list"
-      active={props.todoStore.activeFilter === 'ALL'}
-      onClick={() => props.todoStore.setActiveFilter('ALL')}
+      active={props.todoStore.activeFilter === ALL}
+      onClick={() => props.todoStore.setActiveFilter(ALL)}
     />
 
     <FilterLink
       icon="check_box"
-      active={props.todoStore.activeFilter === 'COMPLETED'}
-      onClick={() => props.todoStore.setActiveFilter('COMPLETED')}
+      active={props.todoStore.activeFilter === COMPLETED}
+      onClick={() => props.todoStore.setActiveFilter(COMPLETED)}
     />
 
     <FilterLink
       icon="check_box_outline_blank"
-      active={props.todoStore.activeFilter === 'UNCOMPLETED'}
-      onClick={() => props.todoStore.setActiveFilter('UNCOMPLETED')}
+      active={props.todoStore.activeFilter === UNCOMPLETED}
+      onClick={() => props.todoStore.setActiveFilter(UNCOMPLETED)}
     />
   </div>
 );
--- src/models/TodoListModel.js
+++ src/models/TodoListModel.js
@@ -1,17 +1,17 @@
 import { observable, computed, action } from 'mobx';
-
+import { COMPLETED, UNCOMPLETED } from '../constants/filters';
 import TodoModel from './TodoModel';
 
 export default class TodoListModel {
   @observable todos = [];
-  @observable activeFilter = 'UNCOMPLETED';
+  @observable activeFilter = UNCOMPLETED;
 
   @computed
   get filteredTodos() {
     switch (this.activeFilter) {
-      case 'COMPLETED':
+      case COMPLETED:
         return this.todos.filter(todo => todo.finished);
-      case 'UNCOMPLETED':
+      case UNCOMPLETED:
         return this.todos.filter(todo => !todo.finished);
       default:
         return this.todos;
