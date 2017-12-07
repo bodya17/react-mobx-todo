c6c84ab1f87d3b2384387c8f0c6ad341bed74b80
bodya17 <vshevchuk@eagleinvsys.com>
2017-12-07 13:29:30 +0200

    make filter component functional

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -4,32 +4,26 @@ import { observer, inject } from 'mobx-react';
 
 import FilterLink from './FilterLink';
 
-@inject(['todoStore'])
-@observer
-class Filter extends Component {
-  render() {
-    return (
-      <div className="todo-filter">
-        <FilterLink
-          icon="list"
-          active={this.props.todoStore.activeFilter === 'ALL'}
-          onClick={() => this.props.todoStore.setActiveFilter('ALL')}
-        />
+const Filter = props => (
+  <div className="todo-filter">
+    <FilterLink
+      icon="list"
+      active={props.todoStore.activeFilter === 'ALL'}
+      onClick={() => props.todoStore.setActiveFilter('ALL')}
+    />
 
-        <FilterLink
-          icon="check_box"
-          active={this.props.todoStore.activeFilter === 'COMPLETED'}
-          onClick={() => this.props.todoStore.setActiveFilter('COMPLETED')}
-        />
+    <FilterLink
+      icon="check_box"
+      active={props.todoStore.activeFilter === 'COMPLETED'}
+      onClick={() => props.todoStore.setActiveFilter('COMPLETED')}
+    />
 
-        <FilterLink
-          icon="check_box_outline_blank"
-          active={this.props.todoStore.activeFilter === 'UNCOMPLETED'}
-          onClick={() => this.props.todoStore.setActiveFilter('UNCOMPLETED')}
-        />
-      </div>
-    );
-  }
-}
+    <FilterLink
+      icon="check_box_outline_blank"
+      active={props.todoStore.activeFilter === 'UNCOMPLETED'}
+      onClick={() => props.todoStore.setActiveFilter('UNCOMPLETED')}
+    />
+  </div>
+);
 
-export default Filter;
+export default inject('todoStore')(observer(Filter));
